e4e03cd9d7e1871d86acf597bd951992d54fcdb0
Firoz Kamdar <firozkamdar@gmail.com>
2017-11-03 09:18:37 -0700

    add item with template complete

--- index_shopping.js
+++ index_shopping.js
@@ -1,9 +1,42 @@
 'use strict';
 
-$(document).ready(function (){
-  $('#js-shopping-list-form').on('submit', function(event)){
+// adding newly entered item to shopping list
+$(document).ready(function() {
+  $('#js-shopping-list-form').on('submit', function(event){
     event.preventDefault();
-    console.log();
-  }
-
+    const entryItem = $('.js-shopping-list-entry').val();
+    const newItem = newItemTemplate(entryItem);
+    //console.log(`${newItem}`);
+    $('.shopping-list').append(newItem);
+  });
 });
+
+function newItemTemplate(entry) {
+  return `
+  <li>
+  <span class="shopping-item">${entry}</span>
+  <div class="shopping-item-controls">
+    <button class="shopping-item-toggle">
+      <span class="button-label">check</span>
+    </button>
+    <button class="shopping-item-delete">
+      <span class="button-label">delete</span>
+    </button>
+  </div>
+</li>
+  `;
+}
+function main() {}
+
+//marking items as checked/unchecked
+function handleCheckedItem() {
+  $('.shopping-list').on('click', 'shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    console.log(`hello`);
+  });
+}
+
+handleCheckedItem();
+
+
+//deleting items
